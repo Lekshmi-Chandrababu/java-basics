d8a8fe479ad5e979409f720e0ee741b2293a8ab5
Lekshmi-Chandrababu <lekshmichandrababu186@gmail.com>
2026-01-12 20:43:01 +0530

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+// Hello World Program
+// Author: Lekshmi Chandrababu
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
